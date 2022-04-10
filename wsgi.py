03faba48088a7408511f94f50ec1405dac54b013
Chantelle Chan <chantelletchan@gmail.com>
2022-04-10 15:10:41 -0700

    modified wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -12,10 +12,10 @@ else:
 flask_env = utils.get_flask_env()
 app = init_app(flask_env, seed_and_exit=seed_and_exit)
 
-
-if __name__ == '__main__':
+app.run()
+# if __name__ == '__main__':
     
-    if flask_env == utils.FlaskEnv.PRODUCTION:
-        app.run(host='0.0.0.0')
-    else:
-        app.run(host='localhost')
+#     if flask_env == utils.FlaskEnv.PRODUCTION:
+#         app.run(host='0.0.0.0')
+#     else:
+#         app.run(host='localhost')
