949d3cb98db96cb15b8511351c116f1cbac8d185
Chantelle Chan <chantelletchan@gmail.com>
2022-04-10 13:01:53 -0700

    added gunicorn production server

--- wsgi.py
+++ wsgi.py
@@ -4,15 +4,17 @@ from app import utils
 import sys
 
 
-if __name__ == '__main__':
-    if '--seed-and-exit' in sys.argv:
-        seed_and_exit = True
-    else:
-        seed_and_exit = False
+if '--seed-and-exit' in sys.argv:
+    seed_and_exit = True
+else:
+    seed_and_exit = False
 
-    flask_env = utils.get_flask_env()
-    app = init_app(flask_env,
-                   seed_and_exit=seed_and_exit)
+flask_env = utils.get_flask_env()
+app = init_app(flask_env, seed_and_exit=seed_and_exit)
+
+
+if __name__ == '__main__':
+    
     if flask_env == utils.FlaskEnv.PRODUCTION:
         app.run(host='0.0.0.0')
     else:
